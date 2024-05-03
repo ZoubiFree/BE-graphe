7f1c46943eca752c149ab5903fdb6a6a3c1f3ede
Zeghari Saad <zegharisaad@gmail.com>
2024-05-03 13:14:12 +0200

    label fini

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -1,13 +1,18 @@
 package org.insa.graphs.algorithm.shortestpath;
 import org.insa.graphs.model.Arc;
-
+import org.insa.graphs.model.Node;
 public class Label {
     private int sommet_courant;
     private boolean marque;
     private int cout_realise;
     private Arc arc_pere;
 
-
+    public Label(int sommet_courant, boolean marque, int cout_realise, Arc arc_pere){
+        this.sommet_courant = sommet_courant;
+        this.marque = marque;
+        this.cout_realise = cout_realise;
+        this.arc_pere = arc_pere;
+    }
     public int getSommet_courant(){
         return sommet_courant;
     }
