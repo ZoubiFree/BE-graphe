46839fe4286e18c5f6184b46b49a2c4af64c3364
Daleo11 <leodalquier@gmail.com>
2024-05-03 12:56:15 +0200

    Label

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -1,5 +1,9 @@
 package org.insa.graphs.algorithm.shortestpath;
+import Arc.java;
 
 public class Label {
-    
+    protected int sommet_courant;
+    protected boolean marque;
+    protected int cout_realise;
+    protected Arc arc_pere;
 }
