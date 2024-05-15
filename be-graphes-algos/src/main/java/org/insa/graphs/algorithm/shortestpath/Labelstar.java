1ac3af665f794502025ba3635960760100a47248
Daleo11 <leodalquier@gmail.com>
2024-05-15 17:33:53 +0200

    cost kikiwi

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Labelstar.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Labelstar.java
@@ -1,5 +1,16 @@
 package org.insa.graphs.algorithm.shortestpath;
 
-public class Labelstar {
-    
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Node;
+import org.insa.graphs.model.Point;
+
+public class Labelstar extends Label {
+    public Labelstar(Node s_courant, boolean marque, boolean reached, float cost, Arc parent) {
+        super(s_courant,marque,reached,cost,parent);
+    }
+
+    public float costkikiwi(Point point_courant,Point point_dest){
+        float dist=((point_courant.getLatitude()-point_dest.getLatitude())**2+(point_courant.getLongitude()-point_dest.getLongitude())**2)**(1/2);
+        return dist;
+    }
 }
