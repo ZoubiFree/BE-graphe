b27f173ff6c5bdfce93c3578f2754ba84007cd79
Daleo11 <leodalquier@gmail.com>
2024-05-15 17:45:32 +0200

    nzbfhzfjkebfjibdjsdfjenfjndzgjnekgbrgnesjvnejbvekbvejnveu voila!

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Labelstar.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Labelstar.java
@@ -3,6 +3,7 @@ package org.insa.graphs.algorithm.shortestpath;
 import org.insa.graphs.model.Arc;
 import org.insa.graphs.model.Node;
 import org.insa.graphs.model.Point;
+import java.lang.Math;
 
 public class Labelstar extends Label {
     public Labelstar(Node s_courant, boolean marque, boolean reached, float cost, Arc parent) {
@@ -10,7 +11,8 @@ public class Labelstar extends Label {
     }
 
     public float costkikiwi(Point point_courant,Point point_dest){
-        float dist=((point_courant.getLatitude()-point_dest.getLatitude())**2+(point_courant.getLongitude()-point_dest.getLongitude())**2)**(1/2);
-        return dist;
+        double dist2=Math.pow(point_courant.getLatitude()-point_dest.getLatitude(),2)+Math.pow(point_courant.getLongitude()-point_dest.getLongitude(),2);
+        double dist=Math.pow(dist2,1/2);
+        return (float)dist;
     }
 }
